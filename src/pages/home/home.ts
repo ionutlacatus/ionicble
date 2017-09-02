45ae5db5aa465da3339fa5384159cab401121e46
ionut <ionut@yahoo.com>
2017-09-02 17:07:55 +0300

    adaugare platforma android si adaugare console log pt start si end constructor;

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BLE } from '@ionic-native/ble';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private ble: BLE) {
+    console.log("inceput de constructor");
+    ble.scan([], 60).subscribe(
+      device => {
+      console.log("Found device: " + JSON.stringify(device));
+      },
+      err => {
+      console.log("Error occurred during BLE scan: " + JSON.stringify(err));
+      },
+      () => {
+      console.log("End of devices...");
+      }
+      );
+      
+    console.log("sf de constructor");
   }
 
 }
